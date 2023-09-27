aa5f1379ce805ea28d4733292587ee19e41e67b2
icashar-g06 <115183848+icashar@users.noreply.github.com>
2023-09-27 10:29:11 -0400

    first

--- Main.java
+++ Main.java
@@ -1,1 +1,7 @@
-public class 
+public class Main
+{
+  public static void main(String[] args) {
+    Spacex S1=new Spacex();//Initialisation d'un objet-comme un scanner, mais au lieux de lire la console ça lis un fichier
+    //S2: Pour crée un nouveau.
+  }  
+}
---
+++ Spacex.java
@@ -0,0 +1,6 @@
+public class Spacex 
+{
+    String heavybooster="Booster 9";
+    String Starship=
+    String lastboosterused= "Falcone 9"
+}
