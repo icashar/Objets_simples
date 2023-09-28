c70a8730029ff4412e7c10fda44095cc96baaebb
icashar-g06 <115183848+icashar@users.noreply.github.com>
2023-09-28 13:20:12 -0400

    done

--- Main.java
+++ Main.java
@@ -3,5 +3,7 @@ public class Main
   public static void main(String[] args) {
     Spacex S1=new Spacex();//Initialisation d'un objet-comme un scanner, mais au lieux de lire la console ça lis un fichier
     //S2: Pour crée un nouveau.
+    S1.nextlaunch(13,4);
+    S1.showInfo();
   }  
 }
--- Spacex.java
+++ Spacex.java
@@ -1,6 +1,21 @@
 public class Spacex 
 {
-    String heavybooster="Booster 9";
-    String Starship=
-    String lastboosterused= "Falcone 9"
+    String booster="HeavyBooster";
+    String starship="Starship";
+    String lastlaunch= "Falcone 9";
+
+    public void showInfo(){
+        System.out.println("The rocket is called "+starship);
+        System.out.println("The first stage is called "+booster);
+        System.out.println("The second stage is called "+starship);
+    }
+    public void nextbooster(int first){
+        System.out.println("The next"+booster+"is"+first);
+    }
+    public void nextstarship(int second){
+        System.out.println("The next"+starship+"is"+second);
+    }
+    public void nextlaunch(int first, int second){
+        System.out.println("The next launch will be "+starship+ second+" with "+booster+ first);
+    }
 }
